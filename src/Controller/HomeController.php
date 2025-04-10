f3ade22c1f6384f4a5735ba3909f004a602bc059
Acynia <valentin.emilie78@gmail.com>
2025-04-10 14:52:24 +0200

    10/04

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\CelestialBodies;
+use App\Entity\CelestialBodyType;
 use App\Repository\CelestialBodiesRepository;
+use App\Repository\CelestialBodyTypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,12 +15,25 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(CelestialBodiesRepository $celestialBodiesRepository): Response
     {
-        // Récupère toutes les entités CelestialBodies
         $celestialBodies = $celestialBodiesRepository->findAll();
 
-        // Passe les données au template
         return $this->render('home/index.html.twig', [
             'celestialBodies' => $celestialBodies,
+        ]);
+    }
+
+    #[Route('/show/{celestialBody<\d+>}', name: 'app_show')]
+    public function show(#[MapEntity(expr: 'repository.find(celestialBodies)')] CelestialBodies $celestialBodies, CelestialBodiesRepository $celestialBodiesRepository, CelestialBodyTypeRepository $celestialBodyTypeRepository): Response
+    {
+        if (!$celestialBodies) {
+            return $this->render('error/404.html.twig');
+        }
+
+        $celestialBodyType = $celestialBodies->getType();
+
+        return $this->render('celestial/show.html.twig', [
+            'celestialBodies' => $celestialBodies,
+            'celestialBodyType' => $celestialBodyType,
         ]);
     }
 }
