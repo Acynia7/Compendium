542348502d9ea5ef6848cb11b035dd24562113c2
Acynia <valentin.emilie78@gmail.com>
2025-05-06 15:55:55 +0200

    API finished

--- src/Entity/CelestialBodyType.php
+++ src/Entity/CelestialBodyType.php
@@ -6,8 +6,10 @@ use App\Repository\CelestialBodyTypeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: CelestialBodyTypeRepository::class)]
+#[ApiResource]
 class CelestialBodyType
 {
     #[ORM\Id]
